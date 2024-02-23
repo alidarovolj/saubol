07e238b5ad1f0012fb7c830ac0ee04fb5e23db4b
alidarovolj <alidarovolzhas@gmail.com>
2024-02-24 04:53:25 +0600

    user profile done

--- layouts/profile.vue
+++ layouts/profile.vue
@@ -1,6 +1,15 @@
 <script setup lang="ts">
-import {IconAddressBook, IconExclamationMark, IconLogout, IconUserCircle, IconVaccine} from "@tabler/icons-vue";
+import {
+  IconAddressBook, IconExclamationMark, IconLogout, IconUserCircle,
+  IconCamera,
+  IconEdit,
+  IconRuler3,
+  IconScaleOutline,
+  IconMathXDivideY
+} from "@tabler/icons-vue";
 import Spinner from "~/components/general/spinner.vue";
+import {useVuelidate} from "@vuelidate/core";
+import {required} from "@vuelidate/validators";
 
 const route = useRoute()
 const router = useRouter()
@@ -8,7 +17,11 @@ const router = useRouter()
 const user = useUserStore()
 const {result} = storeToRefs(user)
 
+const loading = ref(false)
+
 const pending = ref(true)
+
+const editMode = ref(false)
 
 const notify = (type, text) => {
   const toast = useNuxtApp().$toast;
@@ -22,11 +35,62 @@ const logoutLocal = async () => {
   notify(true, 'Вы успешно вышли из профиля')
 }
 
+const form = ref({
+  name: '',
+  iin: '',
+  height: '',
+  weight: ''
+})
+
+const v$ = useVuelidate({
+  name: {required},
+  iin: {required},
+  height: {required},
+  weight: {required}
+}, form);
+
+const formattedName = computed(() => {
+  if (result.value && result.value.data && result.value.data.name) {
+    return result.value.data.name.split(' ').map(word => word.charAt(0).toUpperCase()).join('')
+  }
+  return '';
+});
+
+const doneIMT = computed(() => {
+  if (result.value && result.value.data && result.value.data.user_data) {
+    const {height, weight} = result.value.data.user_data
+    const imt = weight / ((height / 100) * (height / 100))
+    return imt.toFixed(2)
+  }
+  return ''
+})
+
+const updateProfileLocal = async () => {
+  loading.value = true
+  v$.value.$validate();
+
+  if (v$.value.$error) {
+    loading.value = false;
+    return;
+  }
+  await user.updateProfile(form.value)
+  await user.getProfile()
+  loading.value = false
+  editMode.value = false
+}
+
 onMounted(async () => {
   await user.getProfile()
   if (result.value === false) {
     router.push('/')
+    pending.value = false
   } else {
+    form.value.name = result.value.data.name
+    form.value.iin = result.value.data.iin
+    if (result.value.data.user_data) {
+      form.value.height = result.value.data.user_data.height
+      form.value.weight = result.value.data.user_data.weight
+    }
     pending.value = false
   }
 })
@@ -86,12 +150,261 @@ onMounted(async () => {
             </button>
           </div>
         </div>
+        <form
+            @submit.prevent="updateProfileLocal"
+            class="block lg:flex justify-between w-full gap-5 mb-6">
+          <div
+              style="box-shadow: 0px 4px 20px 0px #0000001A;"
+              class="bg-white w-full lg:w-1/3 h-full p-5 rounded-lg">
+            <div class="flex justify-end text-mainColor">
+              <button onclick="setImage.showModal()">
+                <IconEdit/>
+              </button>
+            </div>
+            <div
+                v-if="result.data.img"
+                class="relative h-full"
+            >
+              <img
+                  :src="result.data.img"
+                  class="w-36 h-36 rounded-full object-cover mx-auto"
+                  alt=""
+              >
+            </div>
+            <div
+                v-if="!result.data.img"
+                class="relative w-20 h-full bg-mainColor bg-opacity-20 rounded-lg min-w-20"
+            >
+              <p class="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 text-mainColor text-xl">
+                {{ formattedName }}
+              </p>
+            </div>
+            <h1
+                v-if="!editMode"
+                class="text-xl font-bold text-center mt-3">
+              {{ result.data.name }}
+            </h1>
+            <div
+                v-if="editMode"
+                class="w-1/2 mx-auto mt-3"
+            >
+              <input
+                  v-model="form.name"
+                  :class="{'border-red-500': v$.name.$error}"
+                  class="px-2 py-2 border rounded-lg w-full text-center"
+                  type="text"
+                  placeholder="Введите имя"
+              >
+              <p
+                  v-if="v$.name.$error && editMode"
+                  class="text-red-500 text-xs">
+                Пожалуйста заполните данное поле
+              </p>
+            </div>
+          </div>
+          <div
+              style="box-shadow: 0px 4px 20px 0px #0000001A;"
+              class="mt-5 rounded-lg lg:mt-0 w-full lg:w-2/3 flex flex-col justify-between gap-5 bg-white p-5">
+            <div class="flex items-center justify-between text-mainColor w-full">
+              <h1 class="text-black text-xl font-bold">
+                Мои данные
+              </h1>
+              <div class="flex items-center gap-5">
+                <div
+                    v-if="editMode"
+                    class="w-max">
+                  <button
+                      v-if="!loading"
+                      type="submit"
+                      class="bg-mainColor text-center text-white px-5 py-2 rounded-lg"
+                  >
+                    Обновить
+                  </button>
+                  <p
+                      v-else
+                      class="w-max bg-mainColor text-center text-white px-5 py-2 rounded-lg"
+                  >
+                    <span class="spinner"></span>
+                  </p>
+                </div>
+                <IconEdit
+                    class="cursor-pointer"
+                    size="24"
+                    @click="editMode = !editMode"/>
+              </div>
+            </div>
+            <div class="block lg:flex justify-between gap-5">
+              <div class="mb-3 lg:mb-0 w-full lg:w-1/3 bg-[#ECEDFF] p-3 rounded-lg flex items-center gap-3">
+                <IconRuler3
+                    size="40"
+                    class="text-mainColor"
+                />
+                <div>
+                  <p class="text-[#9A9BA4] text-sm">
+                    Рост
+                  </p>
+                  <input
+                      v-if="editMode"
+                      v-model="form.height"
+                      :class="{'border-red-500': v$.height.$error}"
+                      class="px-2 py-2 border rounded-lg w-full"
+                      type="text"
+                      placeholder="Введите телефон"
+                  >
+                  <p
+                      v-if="v$.height.$error && editMode"
+                      class="text-red-500 text-xs">
+                    Пожалуйста заполните данное поле
+                  </p>
+                  <div v-if="result.data.user_data">
+                    <p
+                        v-if="!editMode"
+                        class="font-medium">
+                      {{ result.data.user_data.height }} см
+                    </p>
+                  </div>
+                  <div
+                      v-else
+                      class="text-red-500"
+                  >
+                    Необходимо заполнить
+                  </div>
+                </div>
+              </div>
+              <div class="mb-3 lg:mb-0 w-full lg:w-1/3 bg-[#ECEDFF] p-3 rounded-lg flex items-center gap-3">
+                <IconScaleOutline
+                    size="40"
+                    class="text-mainColor"
+                />
+                <div>
+                  <p class="text-[#9A9BA4] text-sm">
+                    Вес
+                  </p>
+                  <input
+                      v-if="editMode"
+                      v-model="form.weight"
+                      :class="{'border-red-500': v$.weight.$error}"
+                      class="px-2 py-2 border rounded-lg w-full"
+                      type="text"
+                      placeholder="Введите телефон"
+                  >
+                  <p
+                      v-if="v$.weight.$error && editMode"
+                      class="text-red-500 text-xs">
+                    Пожалуйста заполните данное поле
+                  </p>
+                  <div v-if="result.data.user_data">
+                    <p
+                        v-if="!editMode"
+                        class="font-medium">
+                      {{ result.data.user_data.weight }} кг
+                    </p>
+                  </div>
+                  <div
+                      v-else
+                      class="text-red-500"
+                  >
+                    Необходимо заполнить
+                  </div>
+                </div>
+              </div>
+              <div class="w-full lg:w-1/3 bg-[#ECEDFF] p-3 rounded-lg flex items-center gap-3">
+                <IconMathXDivideY
+                    size="40"
+                    class="text-mainColor"
+                />
+                <div>
+                  <p class="text-[#9A9BA4] text-sm">
+                    ИМТ
+                  </p>
+                  <p
+                      v-if="doneIMT"
+                      class="font-medium">
+                    {{ doneIMT }}
+                  </p>
+                  <p
+                      v-else
+                      class="text-red-500"
+                  >
+                    Заполните вес и рост
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div class="block">
+              <div class="flex items-center justify-between mb-3 lg:mb-5">
+                <p class="text-[#9A9BA4] mb-1">
+                  ИИН
+                </p>
+                <input
+                    v-if="editMode"
+                    v-model="form.iin"
+                    :class="{'border-red-500': v$.iin.$error}"
+                    class="px-2 py-2 border rounded-lg w-full lg:w-1/2"
+                    type="text"
+                    placeholder="Введите ИИН"
+                >
+                <p
+                    v-if="v$.iin.$error && editMode"
+                    class="text-red-500 text-xs">
+                  Пожалуйста заполните данное поле
+                </p>
+                <div v-if="!editMode">
+                  <p v-if="result.data.iin">
+                    {{ result.data.iin }}
+                  </p>
+                  <p v-else class="text-red-500">
+                    Необходимо заполнить
+                  </p>
+                </div>
+              </div>
+              <div class="flex items-center justify-between mb-3 lg:mb-5">
+                <p class="text-[#9A9BA4] mb-1">
+                  Email
+                </p>
+                <p v-if="result.data.email">
+                  {{ result.data.email }}
+                </p>
+                <p v-else class="text-red-500">
+                  Необходимо заполнить
+                </p>
+              </div>
+              <div class="flex items-center justify-between mb-3 lg:mb-0">
+                <p class="text-[#9A9BA4] mb-1">
+                  Телефон
+                </p>
+                <div>
+                  <p v-if="result.data.phone_number">
+                    {{ result.data.phone_number }}
+                  </p>
+                  <p v-else class="text-red-500">
+                    Необходимо заполнить
+                  </p>
+                </div>
+              </div>
+              <!--                    <div class="flex flex-col">-->
+              <!--                      <p class="text-[#9A9BA4] mb-1">-->
+              <!--                        Адрес-->
+              <!--                      </p>-->
+              <!--                      <p v-if="result.data.address">-->
+              <!--                        {{ result.data.address }}-->
+              <!--                      </p>-->
+              <!--                      <p-->
+              <!--                          v-else-->
+              <!--                          class="text-red-500">-->
+              <!--                        Необходимо заполнить-->
+              <!--                      </p>-->
+              <!--                    </div>-->
+            </div>
+          </div>
+        </form>
         <div
+            v-if="route.fullPath !== '/profile'"
             style="box-shadow: 0px 4px 20px 0px #0000001A;"
             class="bg-white px-5 py-6 rounded-lg">
-          <slot />
+          <slot/>
         </div>
-        <VerifyNumber :numPhone="result.data.phone_number" />
+        <VerifyNumber :numPhone="result.data.phone_number"/>
       </div>
       <div v-else class="bg-white px-5 py-6 rounded-lg">
         <Spinner/>
